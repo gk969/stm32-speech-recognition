195d67cb5319fc656363cd41dbbfc4ec562ef207
sj969 <969333221@qq.com>
2017-01-17 01:22:15 +0800

    增加工程文件。优化局部代码结构。

--- FATFS/Interface.h
+++
@@ -1,9 +0,0 @@
-#ifndef __interface_h
-#define __interface_h
-
-void Disk_ReadSec(u32 Addr, u32 *DataBuf);
-void Disk_WriteSec(u32 Addr, u32 *DataBuf);
-
-u16 UnicodeToGb2312(u16 Unicode);
-
-#endif
